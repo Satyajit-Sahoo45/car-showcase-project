b59d4532735af76459937858e9aa9aef0645dc52
Satyajit <sahoosatyajit2801@gmail.com>
2023-10-15 23:41:08 +0530

    added filter options

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
 import Image from 'next/image'
 import { Hero, CustomFilter, SearchBar, CarCard } from '@/components'
 import { fetchCars } from '@/utils'
+import { fuels, yearsOfProduction } from '@/constants';
 
-export default async function Home() {
+export default async function Home({ searchParams }) {
 
-  const allCars = await fetchCars();
+  const allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2023,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || ''
+  });
 
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
 
   return (
     <main className="overflow-hidden">
@@ -23,8 +31,8 @@ export default async function Home() {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter title="fuel" />
-            <CustomFilter title="year" />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
 
--- utils/index.tsx
+++ utils/index.tsx
@@ -1,5 +1,20 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
+export async function fetchCars(filters: FilterProps) {
+    const { manufacturer, year, fuel, limit, model } = filters
+    const headers = {
+        'X-RapidAPI-Key': '7d55684973msh5c6253bb73e0b59p120ef2jsn7ee5cebbb58f',
+        'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
+        headers: headers
+    });
+
+    const result = await response.json();
+
+    return result;
+}
 
 export const calculateCarRent = (city_mpg: number, year: number) => {
     const basePricePerDay = 50; // Base rental price per day in dollars
@@ -29,4 +44,17 @@ export const generateCarImageUrl = (car: CarProps, angle?: string) => {
     url.searchParams.append('angle', `${angle}`);
 
     return `${url}`;
-} 
+}
+
+export const updateSearchParams = (type: string, value: string) => {
+    // Get the current URL search params
+    const searchParams = new URLSearchParams(window.location.search);
+
+    // Set the specified search parameter to the given value
+    searchParams.set(type, value);
+
+    // Set the specified search parameter to the given value
+    const newPathname = `${window.location.pathname}?${searchParams.toString()}`;
+
+    return newPathname;
+}
